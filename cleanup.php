727f1d849603bf3ea55ffcf2b1d4763f5e9a22ce
Sandra <srubioso@hawk.iit.edu>
2013-11-22 10:59:27 -0700

    Update cleanup.php

--- cleanup.php
+++ cleanup.php
@@ -4,17 +4,19 @@
 <title>Cleanup Image</title>
 </head>
 <?php
-// add code to consume the Queue to make sure the job is done
-// add code to send the SMS message of the finished S3 URL
-// Set object expire to remove the image in one day
-// set ACL to public
  session_start();
 ini_set('display_errors',1); 
  error_reporting(E_ALL);
  
 $queueURL = $_SESSION['queueurl'];
+$rcptHandle = $_SESSION['rcpthandle'];
 $domain = $_SESSION['domain'];
-$finishedurl = $_SESSION['finishedurl'] ;
+$finishedurl = $_SESSION['finishedurl'];
+$topicArn = $_SESSION['topicArn'];
+$bucket = $_SESSION['bucket'];
+$aux = $_SESSION['file'];
+$url = $_SESSION['url'];
+$itemName = $_SESSION['itemname'];
  
  // Include the SDK using the Composer autoloader
 require 'vendor/autoload.php';
@@ -30,7 +32,7 @@ use Aws\SimpleDb\Exception\InvalidQueryExpressionException;
  //aws factory
 $aws = Aws::factory('/var/www/vendor/aws/aws-sdk-php/src/Aws/Common/Resources/custom-config.php');
  
- $client = $aws->get('S3'); 
+$client = $aws->get('S3'); 
 
 $sdbclient = $aws->get('SimpleDb'); 
 
@@ -38,43 +40,117 @@ $snsclient = $aws->get('Sns');
 
 $sqsclient = $aws->get('Sqs');
 
+###################################################################
+# Generate S3 link valid for 10 minutes
+##################################################################
+$expires = '+10 minutes';
+
+$expireUrl = $client->getObjectUrl($bucket, $aux, $expires);
+
+###################################################################
+# Short link
+##################################################################
+
+//Server Key
+$apiKey = 'AIzaSyAUeyeKWJZACEJWoFsFVaAgX1O6zQZPayw';
+//Long to Short URL
+$longUrl = $expireUrl;
+$postData = array('longUrl' => $longUrl, 'key' => $apiKey);
+$info = httpsPost($postData);
+	if($info != null)
+	{
+		$shortUrl = $info->id;
+		//echo "Short URL is : ".$info->id."\n";
+	}
+ 
+//Short URL Information
+$shortUrl = $info->id;
+
+ 
+function httpsPost($postData)
+{
+$curlObj = curl_init();
+ 
+$jsonData = json_encode($postData);
+ 
+curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url');
+curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($curlObj, CURLOPT_HEADER, 0);
+curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+curl_setopt($curlObj, CURLOPT_POST, 1);
+curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+ 
+$response = curl_exec($curlObj);
+ 
+//change the response json string to object
+$json = json_decode($response);
+curl_close($curlObj);
+ 
+return $json;
+}
 
 
-
-
-//code to send the SMS message of the finished S3 URL
+###################################################################
+# Send the SMS message of the finished S3 URL
+##################################################################
 
 $result = $snsclient->publish(array(
     'TopicArn' => $topicArn,
     //'TargetArn' => $topicArn,
     // Message is required
-    'Message' => 'Your image has been uploaded',
-    'Subject' => $finishedurl,
+    'Message' => 'Your image has been uploaded and edited',
+    'Subject' => 'Your image has been uploaded and edited  '.$shortUrl,
     'MessageStructure' => 'sms',
 ));
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+###################################################################
+# Consume the Queue to make sure the job is done
+##################################################################
+
+$result = $sqsclient->deleteMessage(array(
+   // QueueUrl is required
+    'QueueUrl' => $queueURL,
+    // ReceiptHandle is required
+    'ReceiptHandle' => $rcptHandle,
+));
+
+
+###################################################################
+# Update 'finishedurl'
+##################################################################
+
+
+$result = $sdbclient->putAttributes(array(
+    // DomainName is required
+    'DomainName' => $domain,
+   // ItemName is required
+    'ItemName' =>$itemName ,
+    // Attributes is required
+    'Attributes' => array(
+         array(
+            'Name' => 'finishedurl',
+            'Value' =>  $shortUrl,
+			'Replace' => true, 
+        ),     
+    ),
+));
+
  ?>
+ 
+ 
+ <body>
+
+
+<!--Display before and after images -->
+
+<h2>Before</h2>
+<img src="<?php echo $url ?>">
+
+<h2>After</h2>
+<img src="<?php echo $shortUrl ?>">
+
+ 
+</body>
+</html>
